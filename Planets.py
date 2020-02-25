6145e4700119f629d8e3cc30f967ed8c418bba0f
Peter-Leimer <peterleimer6@gmail.com>
2020-02-25 08:43:46 -0600

    1st Commit

---
+++ Planets.py
@@ -0,0 +1,11 @@
+class Planet
+
+    
+    def __init__(self, name, designation, diameter):
+         self.name = name
+         self.designation = designation
+         self.diameter = diameter
+
+    def __int__(self):
+         print(name)
+         print(deignation)
