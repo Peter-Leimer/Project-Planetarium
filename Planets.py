bc8e9b2406182e1a47fafdcece5ca3fdd5f8279c
Peter-Leimer <peterleimer6@gmail.com>
2020-02-26 08:33:16 -0600

    Returns Planet

--- Planets.py
+++ Planets.py
@@ -1,11 +1,11 @@
-class Planet
+class Planets(object):
 
     
     def __init__(self, name, designation, diameter):
          self.name = name
          self.designation = designation
          self.diameter = diameter
-
-    def __int__(self):
-         print(name)
-         print(deignation)
+        
+    def __str__(self):
+         return f"Planet: {self.name}  Type: {self.designation}  Diameter: {self.diameter} Km"
+         
