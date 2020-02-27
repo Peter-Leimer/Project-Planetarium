044662397193866c7173893ef112ad34b37ca393
Peter-Leimer <peterleimer6@gmail.com>
2020-02-27 09:30:41 -0600

    Can select planets

--- Main.py
+++ Main.py
@@ -10,11 +10,30 @@ Neptune = Planets("Neptune" , "Gas Giant",49532 )
 Mars = Planets("Mars" , "Rocky-Uninhabitable",6794 )
 Uranus = Planets("Uranus" , "Gas Giant",51118 )
 
-print(str(Mercury))
-print(str(Venus))
-print(str(Earth))
-print(str(Mars))
-print(str(Jupiter))
-print(str(Saturn))
-print(str(Uranus))
-print(str(Neptune))
+selection = str(input("Pick a Planet (By name or type all for the whole solar system): "))
+
+if (selection == "mercury" or selection == "Mercury"):
+       print(str(Mercury))
+elif (selection == "venus" or selection == "Venus"):
+       print(str(Venus))
+elif (selection == "Earth" or selection =="earth"):
+       print(str(Earth))
+elif (selection == "mars" or selection =="Mars"):
+       print(str(Mars))
+elif (selection == "jupiter" or selection == "Jupiter"):
+       print(str(Jupiter))
+elif (selection == "saturn" or selection == "Saturn"):
+       print(str(Saturn))
+elif (selection == "uranus" or selection == "Uranus"):
+       print(str(Uranus))
+elif (selection == "neptune" or selection == "Neptune"):
+       print(str(Neptune))
+elif (selection == "all" or selection == "All"):
+       print(str(Mercury))
+       print(str(Venus))
+       print(str(Earth))
+       print(str(Mars))
+       print(str(Jupiter))
+       print(str(Saturn))
+       print(str(Uranus))
+       print(str(Neptune))
