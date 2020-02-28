759584f546bbfcfb7b686bafe7b705eb64a00488
Peter-Leimer <peterleimer6@gmail.com>
2020-02-28 08:42:41 -0600

    end program start

--- Main.py
+++ Main.py
@@ -10,25 +10,37 @@ Neptune = Planets("Neptune" , "Gas Giant",49532 )
 Mars = Planets("Mars" , "Rocky-Uninhabitable",6794 )
 Uranus = Planets("Uranus" , "Gas Giant",51118 )
 
-selection = str(input("Pick a Planet (By name or type all for the whole solar system): "))
+x=0
 
-if (selection == "mercury" or selection == "Mercury"):
+def selection ():
+  selection = str(input("Pick a Planet (By name or type all for the whole solar system or type end  to end the program): "))
+
+  if (selection == "mercury" or selection == "Mercury"):
        print(str(Mercury))
-elif (selection == "venus" or selection == "Venus"):
+       #selection()
+  elif (selection == "venus" or selection == "Venus"):
        print(str(Venus))
-elif (selection == "Earth" or selection =="earth"):
+       #selection()
+  elif (selection == "Earth" or selection =="earth"):
        print(str(Earth))
-elif (selection == "mars" or selection =="Mars"):
+       #selection()
+  elif (selection == "mars" or selection =="Mars"):
        print(str(Mars))
-elif (selection == "jupiter" or selection == "Jupiter"):
+       #selection()
+  elif (selection == "jupiter" or selection == "Jupiter"):
        print(str(Jupiter))
-elif (selection == "saturn" or selection == "Saturn"):
+       #selection()
+  elif (selection == "saturn" or selection == "Saturn"):
        print(str(Saturn))
-elif (selection == "uranus" or selection == "Uranus"):
+       #selection()
+  elif (selection == "uranus" or selection == "Uranus"):
        print(str(Uranus))
-elif (selection == "neptune" or selection == "Neptune"):
+       #selection()
+  elif (selection == "neptune" or selection == "Neptune"):
        print(str(Neptune))
-elif (selection == "all" or selection == "All"):
+      # selection()
+       
+  elif (selection == "all" or selection == "All"):
        print(str(Mercury))
        print(str(Venus))
        print(str(Earth))
@@ -37,3 +49,17 @@ elif (selection == "all" or selection == "All"):
        print(str(Saturn))
        print(str(Uranus))
        print(str(Neptune))
+       #selection()
+       
+  elif (selection == "end" or selection == "End"):
+       
+       print("Program quit")
+       I=1
+
+
+#while (x==0):
+ # print(selection()) 
+  #  x==1
+print(selection())
+choose = str(input(f"Do it again Y,N"))
+if 
