753223b0ba9eb16dbd22ece209ab9d2fb229b91a
Peter-Leimer <peterleimer6@gmail.com>
2020-02-29 09:44:37 -0600

    WOrking selection

--- Main.py
+++ Main.py
@@ -10,36 +10,35 @@ Neptune = Planets("Neptune" , "Gas Giant",49532 )
 Mars = Planets("Mars" , "Rocky-Uninhabitable",6794 )
 Uranus = Planets("Uranus" , "Gas Giant",51118 )
 
-x=0
+
 
 def selection ():
-  selection = str(input("Pick a Planet (By name or type all for the whole solar system or type end  to end the program): "))
+  selection = str(input("Pick a Planet (By name or type all for the whole solar system ): "))
 
   if (selection == "mercury" or selection == "Mercury"):
        print(str(Mercury))
-       #selection()
+       
   elif (selection == "venus" or selection == "Venus"):
        print(str(Venus))
-       #selection()
+       
   elif (selection == "Earth" or selection =="earth"):
        print(str(Earth))
-       #selection()
+       
   elif (selection == "mars" or selection =="Mars"):
        print(str(Mars))
-       #selection()
+       
   elif (selection == "jupiter" or selection == "Jupiter"):
        print(str(Jupiter))
-       #selection()
+       
   elif (selection == "saturn" or selection == "Saturn"):
        print(str(Saturn))
-       #selection()
+       
   elif (selection == "uranus" or selection == "Uranus"):
        print(str(Uranus))
-       #selection()
+       
   elif (selection == "neptune" or selection == "Neptune"):
        print(str(Neptune))
-      # selection()
-       
+     
   elif (selection == "all" or selection == "All"):
        print(str(Mercury))
        print(str(Venus))
@@ -49,17 +48,19 @@ def selection ():
        print(str(Saturn))
        print(str(Uranus))
        print(str(Neptune))
-       #selection()
        
-  elif (selection == "end" or selection == "End"):
        
-       print("Program quit")
-       I=1
+ 
 
+def choose ():
+  x=0
+  while (x==0):
+    print(selection())
+    choose = str(input(f"choose another? Y,N: "))
+    if (choose == "n" or choose == "N"):
+       x+=1
+       print("QUITER!")
+    elif (choose == "y" or choose == "Y"):
+      choose = "X"
 
-#while (x==0):
- # print(selection()) 
-  #  x==1
-print(selection())
-choose = str(input(f"Do it again Y,N"))
-if 
+print(choose())
